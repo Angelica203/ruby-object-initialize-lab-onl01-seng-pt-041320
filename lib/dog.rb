49865e75925463e06e3801f9e2d8c791ec695833
Angelica Cruz <brantleyhomebuyer@gmail.com>
2020-05-01 01:05:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,16 +10,16 @@ fido = Dog.new("Fido")
 #   def name
 #     @name
 #   end
-# 
+#
 #   def name=(name)
 #     @name = name
 #   end
-# 
+#
 #   def breed=(breed)
 #     @breed = breed
 #   end
-# 
+#
 # end
-# 
+#
 # # Fido = Dog.new("Mutt")
 # # Fido.breed
