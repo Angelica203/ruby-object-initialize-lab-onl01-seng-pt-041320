73439a997926bb70a5a1d324906c70acf87e66c6
Angelica Cruz <brantleyhomebuyer@gmail.com>
2020-05-01 01:05:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,25 @@
 class Dog
 
-  def initialize(name, breed)
+  def initialize(name, breed = 'Mut')
    @name = (name)
   end
-
-  def name
-    @name
-  end
-
-  def name=(name)
-    @name = name
-  end
-
-  def breed=(breed)
-    @breed = breed
-  end
-
 end
 
-# Fido = Dog.new("Mutt")
-# Fido.breed
+fido = Dog.new("Fido")
+
+#   def name
+#     @name
+#   end
+# 
+#   def name=(name)
+#     @name = name
+#   end
+# 
+#   def breed=(breed)
+#     @breed = breed
+#   end
+# 
+# end
+# 
+# # Fido = Dog.new("Mutt")
+# # Fido.breed
